4aa28d38e2d57fda099fdd363569834b181a7e49
aviad339833 <aviad339833@gmail.com>
2023-03-17 21:40:36 -0400

    polished home

--- clients/src/pages/Home.tsx
+++ clients/src/pages/Home.tsx
@@ -33,23 +33,33 @@ function Home({
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="z-[10] relative flex-1">
-        <h1 className="text-[72px] font-bold text-white my-4">
+        <h1 className="text-6xl font-bold bg-gradient-to-r from-white/80 to-white  bg-clip-text text-transparent py-10">
           מה כבר יכול להיות
         </h1>
         <button
-          className="text-white text-[15px] uppercase bg-gradient-to-br from-[#fe3072] to-[#ff5940] py-2 px-4 border-none rounded-[30px] font-semibold transition-all active:translate-y-[1px] hover:from-[#ff5940] hover:to-[#fe3072]"
+          className="text-white text-[15px] uppercase bg-gradient-to-br from-[#fe3072] to-[#9640ff] py-2 px-4 border-none rounded-[30px] font-semibold transition-all active:translate-y-[1px] hover:from-[#ff5940] hover:to-[#fe3072]"
           onClick={handleClick}
         >
           {isAuthenticated ? "התנתק" : "צור חשבון / התחברות"}
         </button>
         {showModal && !isAuthenticated && (
-          <AuthModal
-            cookies={cookies}
-            removeCookie={removeCookie}
-            setCookie={setCookie}
-            setShowModal={setShowModal}
-            isSignUp={isSignUp}
-          />
+          <div
+            className={`fixed top-0 left-0 w-full h-full flex justify-center items-center transition-opacity duration-500 ${
+              isAuthenticated
+                ? "opacity-0 pointer-events-none"
+                : "opacity-100 pointer-events-auto"
+            }`}
+          >
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <AuthModal
+                cookies={cookies}
+                removeCookie={removeCookie}
+                setCookie={setCookie}
+                setShowModal={setShowModal}
+                isSignUp={isSignUp}
+              />
+            </div>
+          </div>
         )}
       </div>
     </div>
