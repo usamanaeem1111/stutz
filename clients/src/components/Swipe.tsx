4eb951546bb54e548dbb6e865a046d8a678e6cea
aviad339833 <aviad339833@gmail.com>
2023-03-21 09:18:04 -0400

    good stage - alert one user about amtches

--- clients/src/components/Swipe.tsx
+++ clients/src/components/Swipe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Transition } from "@headlessui/react";
 import { HiOutlineX, HiOutlineHeart } from "react-icons/hi";
 import axios from "axios";
@@ -14,6 +14,7 @@ const Swipe = ({ cardData, user }: { cardData: Card[]; user: any }) => {
   const [cards, setCards] = useState(cardData);
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [buttonsDisabled, setButtonsDisabled] = useState(false);
+  const [matches, setMatches] = useState<any>([]);
 
   const handleSwipe = async (direction: any) => {
     if (buttonsDisabled) {
@@ -25,8 +26,8 @@ const Swipe = ({ cardData, user }: { cardData: Card[]; user: any }) => {
     setTimeout(() => {
       setButtonsDisabled(false);
     }, 1000);
-    // remove the current card from the array and set the next card as current
-    console.log("direction", direction);
+
+    // Remove the current card from the array and set the next card as current
     const newCards = [...cards];
     const currentCard = newCards[currentCardIndex];
     const nextCardIndex = currentCardIndex + 1;
@@ -49,11 +50,39 @@ const Swipe = ({ cardData, user }: { cardData: Card[]; user: any }) => {
         `${process.env.REACT_APP_BASE_URL}/swipes`,
         data
       );
-      console.log(response);
+      if (response.data && response.data.matched_id) {
+        // Match found, so update matches state
+        setMatches((matches: any) => [
+          ...matches,
+          {
+            user_id: response.data.user_id,
+            corresponding_id: response.data.corresponding_id,
+            matched_id: response.data.matched_id,
+          },
+        ]);
+
+        // Display a notification to the user
+        alert("You have a new match!");
+      }
     } catch (error) {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    // Fetch matches for the user when component mounts
+    const fetchMatches = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}/matches/${user.user_id}`
+        );
+        setMatches(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMatches();
+  }, [user.user_id]);
 
   return (
     <div className="relative w-full flex items-center justify-center h-screen max-w-[480px] ">
@@ -70,7 +99,12 @@ const Swipe = ({ cardData, user }: { cardData: Card[]; user: any }) => {
             leaveTo="opacity-0 scale-95"
           >
             <div className="p-4" key={card.user_id}>
-              <div className="w-full mx-auto mt-10 rounded-lg bg-gradient-to-br from-white/0 to-white backdrop-blur-[7px] shadow-lg">
+              <div className="w-full mx-auto mt-10 rounded-lg bg-gradient-to-br from-black to-black/0 backdrop-blur-[7px] shadow-lg">
+                <img
+                  src={card.images[0]}
+                  alt=""
+                  className="w-full object-cover rounded-lg shadow-lg min-h-[480px]"
+                />
                 <div className="flex justify-between p-4">
                   <button
                     className="p-2 mx-2 text-white rounded-full hover:bg-black/50 hover:text-white focus:outline-none focus:ring-2 focus:ring-gray-600 active:translate-y-[1px]"
@@ -78,7 +112,7 @@ const Swipe = ({ cardData, user }: { cardData: Card[]; user: any }) => {
                   >
                     <HiOutlineX size={36} />
                   </button>
-                  <h2 className="mt-2 text-lg font-medium text-gray-800">
+                  <h2 className="mt-2 text-2xl font-medium text-white">
                     {card.first_name}
                   </h2>
                   <button
@@ -88,12 +122,6 @@ const Swipe = ({ cardData, user }: { cardData: Card[]; user: any }) => {
                     <HiOutlineHeart size={36} />
                   </button>
                 </div>
-
-                <img
-                  src={card.images[0]}
-                  alt=""
-                  className="w-full object-cover rounded-lg shadow-lg min-h-[480px]"
-                />
               </div>
             </div>
           </Transition>
